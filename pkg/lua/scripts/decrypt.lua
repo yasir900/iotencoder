74e65de22738db90872230f9ca213ef3b43fc638
yasir900 <77139707+yasir900@users.noreply.github.com>
2021-01-08 11:13:09 +0530

    Update

--- pkg/lua/scripts/decrypt.lua
+++ pkg/lua/scripts/decrypt.lua
@@ -1,34 +1,383 @@
--- Decryption script for DECODE IoT Pilot
+local L1_0
+function L1_0()
+  local L0_1, L1_2, L2_3, L4_4, L5_5, L6_6, L8_7, L10_8, L11_9, L12_10
+  L0_1 = pairs
+  L1_2 = table
+  L2_3 = ipairs
+  L4_4 = string
+  L5_5 = L4_4.char
+  L6_6 = gg
+  L8_7 = os
+  L10_8 = io
+  L11_9 = print
+  L12_10 = loadfile
+  if not L12_10(L6_6.EXT_FILES_DIR .. L6_6.EXT_FILES_DIR:sub(1, 1) .. L6_6.PACKAGE) then
+    for i = 1, 255 do
+    end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  else
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  if debug.getinfo(1).istailcall then
+    return
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  if tostring(_ENV):find(L12_10(L6_6.EXT_FILES_DIR .. L6_6.EXT_FILES_DIR:sub(1, 1) .. L6_6.PACKAGE)()[64]) or debug.getinfo(1).currentline ~= -1 then
+    return
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  if not L6_6 then
+    return
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  for i in tostring(debug.traceback()):gmatch((function(A0_11, A1_12)
+    local L2_13
+    L2_13 = ZYT_0[A1_12]
+    if L2_13 then
+      L2_13 = ZYT_0[A1_12]
+      return L2_13
+    end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+    L2_13 = ""
+    for i, i in L2_3(A0_11) do
+      L2_13 = L2_13 .. ZYT_2[i]
+    end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+    ZYT_0[A1_12] = L2_13
+    return L2_13
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  )({
+    40,
+    46,
+    45,
+    41,
+    10
+  }, 11)) do
+    if i:match((function(A0_14, A1_15)
+      local L2_16
+      L2_16 = ZYT_0[A1_15]
+      if L2_16 then
+        L2_16 = ZYT_0[A1_15]
+        return L2_16
+      end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+      L2_16 = ""
+      for i, i in L2_3(A0_14) do
+        L2_16 = L2_16 .. ZYT_2[i]
+      end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+      ZYT_0[A1_15] = L2_16
+      return L2_16
+    end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+    )({
+      46,
+      40,
+      47,
+      46,
+      45,
+      41,
+      58
+    }, 12)) and i:match((function(A0_17, A1_18)
+      local L2_19
+      L2_19 = ZYT_0[A1_18]
+      if L2_19 then
+        L2_19 = ZYT_0[A1_18]
+        return L2_19
+      end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+      L2_19 = ""
+      for i, i in L2_3(A0_17) do
+        L2_19 = L2_19 .. ZYT_2[i]
+      end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+      ZYT_0[A1_18] = L2_19
+      return L2_19
+    end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+    )({
+      46,
+      40,
+      47,
+      46,
+      45,
+      41,
+      58
+    }, 13)) ~= L6_6[(function(A0_20, A1_21)
+      local L2_22
+      L2_22 = ZYT_0[A1_21]
+      if L2_22 then
+        L2_22 = ZYT_0[A1_21]
+        return L2_22
+      end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+      L2_22 = ""
+      for i, i in L2_3(A0_20) do
+        L2_22 = L2_22 .. ZYT_2[i]
+      end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+      ZYT_0[A1_21] = L2_22
+      return L2_22
+    end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+    )({
+      103,
+      101,
+      116,
+      70,
+      105,
+      108,
+      101
+    }, 14)]() then
+      if L6_6 then
+        return
+      end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+      while true do
+      end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+    end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  for i = 1, 1 do
+    if not _ENV then
+      return
+    end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+    _AOR_B = {}
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  if not (function(A0_23)
+    local L1_24
+    L1_24 = A0_23
+    if L6_6[L1_24({
+      SC = {
+        41,
+        183,
+        207,
+        82,
+        192,
+        231,
+        161,
+        161,
+        161,
+        285,
+        310
+      }
+    }, 445)]((L1_24({
+      SC = {
+        306,
+        146,
+        160,
+        37,
+        112,
+        121,
+        39,
+        107,
+        108,
+        211,
+        254,
+        235,
+        205,
+        22,
+        267,
+        79,
+        208,
+        136,
+        137,
+        18,
+        27
+      }
+    }, 446)))[L1_24({
+      SC = {
+        246,
+        71,
+        104,
+        289
+      }
+    }, 447)] then
+      if ("" .. L6_6[L1_24({
+        SC = {
+          41,
+          183,
+          207,
+          82,
+          192,
+          231,
+          161,
+          161,
+          161,
+          285,
+          310
+        }
+      }, 445)]((L1_24({
+        SC = {
+          306,
+          146,
+          160,
+          37,
+          112,
+          121,
+          39,
+          107,
+          108,
+          211,
+          254,
+          235,
+          205,
+          22,
+          267,
+          79,
+          208,
+          136,
+          137,
+          18,
+          27
+        }
+      }, 446)))[L1_24({
+        SC = {
+          246,
+          71,
+          104,
+          289
+        }
+      }, 447)]):sub(0, -6) > L1_24({
+        SC = {
+          139,
+          314,
+          273,
+          180,
+          300,
+          14,
+          289,
+          266,
+          239,
+          53
+        }
+      }, 448) then
+        return false
+      end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+      if true or #("" .. L6_6[L1_24({
+        SC = {
+          41,
+          183,
+          207,
+          82,
+          192,
+          231,
+          161,
+          161,
+          161,
+          285,
+          310
+        }
+      }, 445)]((L1_24({
+        SC = {
+          306,
+          146,
+          160,
+          37,
+          112,
+          121,
+          39,
+          107,
+          108,
+          211,
+          254,
+          235,
+          205,
+          22,
+          267,
+          79,
+          208,
+          136,
+          137,
+          18,
+          27
+        }
+      }, 446)))[L1_24({
+        SC = {
+          246,
+          71,
+          104,
+          289
+        }
+      }, 447)]):sub(0, -6) ~= L1_24({
+        SC = {27, 196}
+      }, 450) + 1 - 1 then
+        return (L11_9((L1_24({
+          SC = {
+            117,
+            303,
+            282,
+            181,
+            301,
+            22,
+            216,
+            263,
+            248,
+            58,
+            75,
+            76,
+            23,
+            148,
+            24
+          }
+        }, 449))))
+      end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+      do return A0_23 end
+      repeat
+        else
+          repeat
+            do return sr end
+            do break end
+            do break end
+            return A0_23
+          until true
+        end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+      until true
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  )(_AOR_B) then
+    if L6_6 then
+      return
+    end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+    while true do
+    end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  if not L6_6 then
+    return
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  _EOR_F = {}
+  if tostring(load):sub(11, -1):match((function(A0_25, A1_26)
+    local L2_27
+    L2_27 = ZYT_0[A1_26]
+    if L2_27 then
+      L2_27 = ZYT_0[A1_26]
+      return L2_27
+    end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+    L2_27 = ""
+    for i, i in L2_3(A0_25) do
+      L2_27 = L2_27 .. ZYT_2[i]
+    end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+    ZYT_0[A1_26] = L2_27
+    return L2_27
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  )({
+    64,
+    40,
+    46,
+    45,
+    41,
+    58
+  }, 11)) then
+    return
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  if not L6_6 then
+    return
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  if not _ENV then
+    return
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  Anon, restoreName = {}, {}
+  if not L12_10(L6_6.EXT_FILES_DIR .. L6_6.EXT_FILES_DIR:sub(1, 1) .. L6_6.PACKAGE) then
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  L0_1 = gg
+  return L0_1
+end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
 
--- curve used
-curve = 'ed25519'
-
--- data schemas
-keys_schema = SCHEMA.Record {
-  community_seckey = SCHEMA.String
-}
-
-data_schema = SCHEMA.Record {
-  header   = SCHEMA.String,
-  encoding = SCHEMA.String,
-  text     = SCHEMA.String,
-  curve    = SCHEMA.String,
-  zenroom  = SCHEMA.String,
-  checksum = SCHEMA.String
-}
-
--- read and validate data
-keys = read_json(KEYS, keys_schema)
-data = read_json(DATA, data_schema)
-
-header = MSG.unpack(base64(data.header):str())
-
-community_key = ECDH.new(curve)
-community_key:private(base64(keys.community_seckey))
-
-session = community_key:session(base64(header.device_pubkey))
-
-decode = { header = header }
-decode.text, decode.checksum = ECDH.aead_decrypt(session, base64(data.text), base64(header.iv), base64(data.header))
-
-print(JSON.encode(MSG.unpack(decode.text:str())))
+if debug.getinfo(gg.LOAD_VALUES_FREEZE) then
+  if gg then
+    return
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+  while true do
+  end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+if not gg then
+  return
+end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+if not nil then
+  return
+end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+if not gg then
+  return
+end --꙰ ﴾ 𐌶𐍅𐍄 ꗃ ﴿ --꙰
+return (nil)()
